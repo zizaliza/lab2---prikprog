fb6462615f75747b718450692cbbaf60e40e15d7
zizaliza <lisakruzh@gmail.com>
2024-11-29 23:20:55 +0300

    Добавили консольный интерфейс

---
+++ main.py
@@ -0,0 +1,55 @@
+from requests import RequestException
+from regex_service import RegexService
+from record_manager import JsonRecordRepository
+
+
+def main():
+    repository = JsonRecordRepository()
+    service = RegexService(repository)
+
+    while True:
+        print("\nВыберите действие:")
+        print("1. Получить дату из файла")
+        print("2. Получить дату с веб-страницы")
+        print("3. Найти дату в тексте")
+        print("4. Выход")
+
+        try:
+            user_input = input("Введите номер действия: ").strip().lower()
+            choice = int(user_input)
+
+            match choice:
+                case 1:
+                    record = service.get_dates_in_file()
+                        
+                    print(f"Результат из файла: {record.date}")
+
+                case 2:
+                    url = input("Введите URL веб-страницы: ")
+                    record = service.get_dates_in_web(url)
+
+                    print(f"Результат с веб-страницы: {record.dates}")
+
+                case 3:
+                    text = input("Введите текст: ")
+                    dates_list = service.get_dates_in_text(text)
+
+                    print(f"Результат из текста: {dates_list}")
+
+                case 4:
+                    print("Выход из программы.")
+                    break
+
+                case _:
+                    print("Ошибка: выбрано недопустимое действие. Попробуйте снова.")
+
+        except RequestException as e:
+            print(f"Ошибка при выполнении запроса: {e}")
+        except KeyboardInterrupt:
+            print("\nВыход из программы.")
+            break
+        except Exception as e:
+            print(f"Произошла ошибка: {e}")
+
+if __name__ == "__main__":
+    main()
--- regex_service.py
+++ regex_service.py
@@ -16,13 +16,13 @@ class RegexService:
         self.record_repository = record_repository
         pass
 
-    def _find_snils(self, text: str) -> List[str]:
+    def _find_dates(self, text: str) -> List[str]:
         return re.findall(PATTERN, text)
     
-    def get_snils_in_file(self):
+    def get_dates_in_file(self):
         record_data = self.record_repository.get_all()
         record_str = ", ".join([str(asdict(record)) for record in record_data])
-        date = self._find_snils(record_str)
+        date = self._find_dates(record_str)
         return Record(link="jsonFile", date=date)
 
     def get_dates_in_web(self, url: str):
@@ -39,8 +39,6 @@ class RegexService:
             print(f"Error: {e}")
             return Record(link=url, dates=[])
 
-    def get_snils_in_text(self, text: str = "") -> List[str]:
+    def get_dates_in_text(self, text: str = "") -> List[str]:
         dates = self._find_dates(text)
-        self.record_repository.add(Record(link='text', dates=dates))
         return dates
-s
